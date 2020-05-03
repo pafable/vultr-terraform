3666937509e566353f1f6050c314c5e3e7f75859
Philip Afable <philafable@gmail.com>
2020-05-02 23:38:59 -0400

    fixed startup

--- startup.tf
+++ startup.tf
@@ -1,5 +1,4 @@
 resource "vultr_startup_script" "my_startup" {
     name = "init.sh"
-    script = "yum update -y && yum upgrade -y
-    yum install wget net-tools vim -y"
+    script = "yum update -y && yum upgrade -y && yum install wget net-tools vim -y"
 }

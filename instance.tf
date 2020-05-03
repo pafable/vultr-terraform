6b51f9363d85a65f62818a4b8040ecc1913bf8ca
Philip Afable <philafable@gmail.com>
2020-05-02 23:36:56 -0400

    added startup script

--- instance.tf
+++ instance.tf
@@ -5,6 +5,7 @@ resource "vultr_server" "tf_vultr_srv" {
   os_id = "${data.vultr_os.my_os.id}"
   plan_id = "${data.vultr_plan.my_plan.id}"
   region_id = "${data.vultr_region.my_region.id}"
+  script_id = "${var.vultr_startup_script.my_startup.id}"
   ssh_key_ids = "${var.ssh_key.id}"
   tag = "${var.instance_tag}"
   notify_activate = true
